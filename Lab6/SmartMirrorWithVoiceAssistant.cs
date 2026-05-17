f81acf5712e3d3c0834543feb15c9d586b0baf28
zvarich2025 <zvarich2025@gmail.com>
2026-05-17 20:03:31 +0300

    Create SmartMirrorWithVoiceAssistant.cs

---
+++ Lab6/SmartMirrorWithVoiceAssistant.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+namespace SmartMirrorLab6
+{
+    // ПОХІДНИЙ КЛАС: РОЗУМНИЙ ГОЛОСОВИЙ ПОМІЧНИК (ЗАВДАННЯ 3 - НАСЛІДУВАННЯ)
+    public class SmartMirrorWithVoiceAssistant : SmartMirror
+    {
+        private string _assistantName;
+
+        public SmartMirrorWithVoiceAssistant(List<User> externalUsers, int initialBattery, string name)
+            : base(externalUsers, initialBattery)
+        {
+            _assistantName = name;
+        }
+
+        public void InterpretVoiceInput(string voiceCommand, int userIndex)
+        {
+            Console.WriteLine($"\n[Голосовий модуль] Почуто команду: \"{voiceCommand}\"");
+            Console.WriteLine($"[{_assistantName}] Виконую обробку мовного запиту...");
+
+            if (voiceCommand.Contains("сканування") || voiceCommand.Contains("діагностика"))
+            {
+                AnalyzeUserReflection(userIndex);
+            }
+            else if (voiceCommand.Contains("зачіска"))
+            {
+                ExecuteHairstyleSimulation();
+            }
+            else if (voiceCommand.Contains("фото"))
+            {
+                camera.TakePhoto();
+                network.BroadcastCloudData("Image_RAW_092.png", "Хмарне Сховище Друзів");
+            }
+            else
+            {
+                Console.WriteLine($"[{_assistantName}] Команда \"{voiceCommand}\" не підтримується поточною прошивкою.");
+            }
+        }
+
+        // Реалізація обробника події критичної ситуації всередині помічника
+        public void SpeakEmergencyWarning(string alertText, User user)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"\n[{_assistantName} - МОВНИЙ СИНТЕЗАТОР ШОКОВОЇ СИТУАЦІЇ]: Увага!");
+            Console.WriteLine($"[{_assistantName}]: Користувач {user.Name} знепритомнів або має критичний пульс ({user.Pulse} уд/хв). Запускаю протокол порятунку!");
+            Console.ResetColor();
+        }
+    }
+}
