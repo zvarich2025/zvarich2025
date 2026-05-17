896a579e4596a6db6cfd0bdcd63c03a1e6bf6fbb
zvarich2025 <zvarich2025@gmail.com>
2026-05-17 20:01:10 +0300

    Create CriticalMedicalEmergencyException.cs

---
+++ Lab6/CriticalMedicalEmergencyException.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace SmartMirrorLab6
+{
+    // Медичне виключення: користувач потрапив у критичну ситуацію (інфаркт, інсульт, непритомність)
+    public class CriticalMedicalEmergencyException : Exception
+    {
+        public double Temperature { get; }
+        public int Pulse { get; }
+
+        public CriticalMedicalEmergencyException(string message, double temperature, int pulse) : base(message)
+        {
+            Temperature = temperature;
+            Pulse = pulse;
+        }
+    }
+}
